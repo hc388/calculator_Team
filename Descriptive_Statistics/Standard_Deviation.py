1a8495d1892bb67d55052bdac391f406cc8a7fb5
Hrithik <hc388@njit.edu>
2020-05-16 01:27:02 -0400

    Task: Standard_Deviation.py

---
+++ Descriptive_Statistics/Standard_Deviation.py
@@ -0,0 +1,21 @@
+from MathOperations.Addition import Addition
+from MathOperations.Division import Division
+from Descriptive_Statistics.Mean import Mean
+from MathOperations.Exponentiation import Exponentiation
+from MathOperations.nthRoot import nthRoot
+
+
+class Standard_Deviation:
+
+    @staticmethod
+    def deviation_Calculator(data):
+        newdata = []
+        data.sort()
+        n = len(data)
+        mean = Mean.Mean_Calculator(data)
+        for i in data:
+            newdata.append(Exponentiation.power((i - mean),2))
+
+        sum = Addition.sumList(newdata)
+        return nthRoot.rooting(2,Division.divide(sum,n))
+
